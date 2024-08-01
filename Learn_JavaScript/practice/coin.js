69795003065fe996ec480162af1e46c93b6dca79
Srijit Dey <deysrijit2@gmail.com>
2024-08-02 00:05:32 +0530

    update

---
+++ Learn_JavaScript/practice/coin.js
@@ -0,0 +1,7 @@
+let num = Math.random();
+
+if (num > 0.5) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}
---
+++ Learn_JavaScript/practice/good-morning.js
@@ -0,0 +1,7 @@
+let hour = 5
+
+if (hour <= 12){
+    console.log("Good Morning!")
+}else{
+    console.log("Wake up early")
+}
